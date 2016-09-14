e0dbc1729b558965e313715959ccc0f0f5bbd618
Juan Antonio <juanan.echeve@gmail.com>
2016-09-14 23:40:00 +0200

    Update OnlineVideoTutorialsApp.java

--- src/main/java/org/jaea/onlinevideotutorials/OnlineVideoTutorialsApp.java
+++ src/main/java/org/jaea/onlinevideotutorials/OnlineVideoTutorialsApp.java
@@ -1,57 +1,52 @@
-/*
- * (C) Copyright 2014 Kurento (http://kurento.org/)
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- */
-package org.jaea.onlinevideotutorials;
-
-import org.kurento.client.KurentoClient;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-/**
- * Online Video Tutorials App main class.
- * 
- * @author Juan Antonio Echeverrías Aranda (juanan.echeve@gmail.com)
- */
-@Configuration
-@EnableWebSocket
-@EnableAutoConfiguration
-public class OnlineVideoTutorialsApp implements WebSocketConfigurer {
-
-    final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
-
-    @Bean
-    public OnlineVideoTutorialsHandler handler() {
-        return new OnlineVideoTutorialsHandler();
+    <html>
+      <head>
+        <script>
+            document.write('<base href="' + document.location + '" />');
+        </script>  
         
-    }
-
-    @Bean
-    public KurentoClient kurentoClient() {
-        return KurentoClient.create(System.getProperty("kms.ws.uri", DEFAULT_KMS_WS_URI));
-    }
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(handler(), "/ovt");
-    }
-
-    public static void main(String[] args) throws Exception {
-        new SpringApplication(OnlineVideoTutorialsApp.class).run(args);
-    }
-}
+        
+        <title>Online Video Tutorials</title>
+        
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,
+              maximum-scale=1.0, minimum-scale=1.0">
+        
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        
+        
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="bower_components/ekko-lightbox/dist/ekko-lightbox.min.js"></script>
+        <script src="bower_components/adapter.js/adapter.js"></script>
+        
+         <script src="bower_components/demo-console/index.js"></script>
+        <script src="bower_components/kurento-utils/js/kurento-utils.js"></script>
+        
+            <!-- 1. Load libraries -->
+     <!-- Polyfill(s) for older browsers -->
+        <script src="node_modules/core-js/client/shim.min.js"></script>
+        <script src="node_modules/zone.js/dist/zone.js"></script>
+        <script src="node_modules/reflect-metadata/Reflect.js"></script>
+        <script src="node_modules/systemjs/dist/system.src.js"></script>
+        <!-- 2. Configure SystemJS -->
+        <script src="systemjs.config.js"></script>
+        <script>
+          System.import('app').catch(function(err){ console.error(err); });
+        </script>
+  </head>
+        
+      <body>
+          <header></header>
+           
+          <app>Loading...</app>
+        <!-- <script src="https://github.jspm.io/jmcriffey/bower-traceur-runtime@0.0.87/traceur-runtime.js"></script> 
+        <script src="https://code.angularjs.org/tools/system.js"></script>
+        <script src="https://code.angularjs.org/tools/typescript.js"></script>
+        <script src="https://code.angularjs.org/2.0.0-alpha.44/angular2.dev.js"></script>
+        <script src="https://code.angularjs.org/2.0.0-alpha.44/router.dev.js"></script>
+       -->
+       <footer class="panel-footer">
+        <p>JAEA</p> 
+       </footer>
+       
+      </body>
+    </html>
