6873d6081a552f0e76e4ac309be8f0dc69a58120
Juan Antonio <juanan.echeve@gmail.com>
2016-09-14 23:42:24 +0200

    Update UserSessionsRegistry.java

--- src/main/java/org/jaea/onlinevideotutorials/UserSessionsRegistry.java
+++ src/main/java/org/jaea/onlinevideotutorials/UserSessionsRegistry.java
@@ -15,165 +15,119 @@
 package org.jaea.onlinevideotutorials;
 
 import com.google.gson.JsonObject;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.WebSocketSession;
 
 public class UserSessionsRegistry {
     
     private final Logger log = LoggerFactory.getLogger(UserSessionsRegistry.class);
     
-    private final ConcurrentHashMap<String, UserSession> usersByName = new ConcurrentHashMap<String, UserSession>();
-    private final ConcurrentHashMap<String, UserSession> usersBySessionId = new ConcurrentHashMap<String, UserSession>();
-    private final ConcurrentHashMap<String, List<UserSession>> studentsByRoomName = new ConcurrentHashMap<String, List<UserSession>>();
-    private final ConcurrentHashMap<String, UserSession> tutorsByRoomName = new ConcurrentHashMap<String, UserSession>();
-    //private final ConcurrentHashMap<String, UserSession> participantsByRoomName = new ConcurrentHashMap<String, UserSession>();
+    /* The next two collections are going to store all the users */
+    private final ConcurrentHashMap<String, UserSession> usersByUserName = new ConcurrentHashMap();
+    private final ConcurrentHashMap<String, UserSession> usersBySessionId = new ConcurrentHashMap();
     
-    public UserSessionsRegistry(){
+    /* It stores the students which are not in a room */
+    private final ConcurrentHashMap<String, UserSession> incomingParticipantsByUserName = new ConcurrentHashMap();
+    
+    public boolean isThereAlreadyThisUser(String userName) {
         
+        return this.usersByUserName.containsKey(userName);
     }
     
     public void addUser (UserSession user){
-        log.info("* addUser");
+        log.info("{} UserRegistry.addUser: {}, {}", Info.START_SYMBOL, user.getUserName(), Hour.getTime());
         
-        usersByName.put(user.getUserName(), user);
-        usersBySessionId.put(user.getSession().getId(), user);
+        this.usersByUserName.put(user.getUserName(), user);
+        log.info(" add to usersByName");
+        this.usersBySessionId.put(user.getSession().getId(), user);
+        log.info(" add to usersBySessionId");
         
-        String room = user.getRoomName();
-        
-        if (user.getUserType().equals(UserSession.TUTOR_TYPE)){
-            tutorsByRoomName.put(room, user);
-            log.info("a tutor has been added");
-        }
-        else{
-            
-            if (!studentsByRoomName.containsKey(room)){
-                studentsByRoomName.put(room, new ArrayList<UserSession>());
-            }
-            studentsByRoomName.get(room).add(user);
-            log.info("a student has been added");
-        }  
-        
-    }
-    
-    /*
-    public void addUserIntoRoom (UserSession user){
-        
-        String room = user.getRoomName();
-        
-        if (user.getUserType().equals(UserSession.TUTOR_TYPE)){
-            tutorsByRoomName.put(room, user);
-        }
-        else{
-            
-            if (!studentsByRoomName.containsKey(room)){
-                studentsByRoomName.put(room, new ArrayList<UserSession>());
-            }
-            studentsByRoomName.get(room).add(user);
-        }    
-        
-    }
-    */
-    
-    public List<UserSession> getStudentsByRoomName(String roomName){
-        return studentsByRoomName.get(roomName);
-    }
-    
-    /**
-     * 
-     * @param roomName
-     * @return List<String> or null 
-     */
-    public List<String> getStudentsNameByRoomName(String roomName){
-        
-        List <UserSession> students = studentsByRoomName.get(roomName);
-        List <String> studentsNames = null;
-        if (students != null){
-            
-            studentsNames = new ArrayList<String>();
-            for(UserSession user : students){
-                studentsNames.add(user.getUserName());
-            }
-            
-        }    
-        return studentsNames;
-    }
-    
-    public void sendAMessageToTheTutor(JsonObject message, String room){
-        log.info("* sendAMessageToTheTutor - message: {}", message.get("id").getAsString());
-        
-        UserSession tutor = getTutorOfRoom(room);
-        
-        if (tutor!=null){
-            
-            tutor.sendMeAMessage(message);
-            
+        if (user.isAStudent()) {
+            this.addIncomingParticipant(user);
         }
         
-        log.info("the message has been sent to the tutor of the '{}' room ", room);
+        Info.logInfoFinish("UserRegistry.addUser");
     }
     
-    public void sendAMessageToAllStudentsOfRoom(JsonObject message, String room){
-        log.info("* sendAMessageToAllStudentsOfRoom - message: {}, room: {}", message.get("id").getAsString(), room);
+    public void addIncomingParticipant (UserSession user){
+        log.info("{} UserRegistry.addIncomingParticipant: {} {}",Info.START_SYMBOL, user.getUserName(), Hour.getTime());
         
-        List<UserSession> students = getStudentsByRoomName(room);
+        if (user.isAStudent()){
+            incomingParticipantsByUserName.put(user.getUserName(), user);
+            log.info("An incoming participant has been added");
+            log.info("Now there are {} students in the waiting room",incomingParticipantsByUserName.size());
+        }
         
-        if (students!=null){
+        Info.logInfoFinish("UserRegistry.addIncomingParticipant");
+     }
+    
+    public UserSession getUserByUserName(String userName){
+        log.info("{} UserRegistry.getUserByUserName: {} {}", Info.START_SYMBOL,userName, Hour.getTime());
+        Info.logInfoFinish("UserRegistry.getUserByUserName");
+        
+        return usersByUserName.get(userName);
+    }
+    
+    public UserSession getUserBySessionId(String sessionId){
+        log.info("{} UserRegistry.getUserBySessionId: {} {}", Info.START_SYMBOL,sessionId, Hour.getTime());
+        Info.logInfoFinish("UserRegistry.getUserBySessionId: " + usersBySessionId.get(sessionId));
+        
+        return usersBySessionId.get(sessionId);
+    }
+    
+    public UserSession getIncomingParticipantByUserName(String userName){
+        log.info("{} UserRegistry.getIncomingParticipantByUserName: {} {}", Info.START_SYMBOL,userName, Hour.getTime());
+        Info.logInfoFinish("UserRegistry.getIncomingParticipantByUserName");
+        
+        return incomingParticipantsByUserName.get(userName);
+    }
+    
+    public UserSession removeIncomingParticipant(UserSession user){
+        log.info("{} UserRegistry.removeIncomingParticipant: {} {}",Info.START_SYMBOL, user.getUserName(), Hour.getTime());
+         
+       UserSession incomingParticipant = this.removeIncomingParticipant(user.getUserName());
+         
+        Info.logInfoFinish("UserRegistry.removeIncomimgParticipant");
+        
+        return incomingParticipant;
+    }
+    
+    public UserSession removeIncomingParticipant(String userName){
+        log.info("{} UserRegistry.removeIncomingParticipant: {} {}",Info.START_SYMBOL, userName, Hour.getTime());
+         
+        UserSession user = this.incomingParticipantsByUserName.remove(userName);
+        
+        log.info("An incoming participant has been removed");
+        log.info("Now there are {} students in the waiting room",incomingParticipantsByUserName.size());
+        Info.logInfoFinish("UserRegistry.removeIncomimgParticipant");
+        
+        return user;
+    }
+    
+    public void removeUser (String userName){
+        log.info("{} UserRegistry.removeUser: {}, {}", Info.START_SYMBOL, userName, Hour.getTime());
+        
+        UserSession outgoingUser = this.usersByUserName.remove(userName);
+        this.usersBySessionId.remove(outgoingUser.getSessionId());
+        this.incomingParticipantsByUserName.remove(userName);
+    }
+    
+    public void sendAMessageToIncomingParticipants(JsonObject message){
+        log.info("{} UserRegistry.sendAMessageToIncomingParticipants - message: {} {}",Info.START_SYMBOL, message, Hour.getTime());
+        
+        if (this.incomingParticipantsByUserName!=null){
             
-            for (UserSession student : students){
-                log.info("Student: {}", student.getSession().getId());
+            for (UserSession user : Collections.list(incomingParticipantsByUserName.elements())){
+                log.info("User: {}", user.getUserName());
 
-                student.sendMeAMessage(message);
+                user.sendMeAMessage(message);
             }  
             
         }
         
-        log.info("the message has been sent to all students of the '{}' room ", room);
+        log.info("the message has been sent to all incoming participants");
+        Info.logInfoFinish("UserRegistry.sendAMessageToIncomingParticipants");
     }
-    
-    public List<String> getRoomNames (){
-        return Collections.list(tutorsByRoomName.keys());
-    }
-    
-    public UserSession getTutorOfRoom (String roomName){
-        return tutorsByRoomName.get(roomName);
-    }
-    
-    public String getTutorNameOfRoom (String roomName){
-        return tutorsByRoomName.get(roomName).getUserName();
-    }
-    
-    /**
-     * 
-     * @param id 
-     */
-    public void removeUserFromThisRoom(String id){
-        log.info("* removeUserFromRoom - id: {}", id);
-        log.info("users: {}", usersBySessionId.toString()); 
-        
-        UserSession user = usersBySessionId.get(id);
-        if (user == null){
-            log.info("user is null");
-        }
-        log.info("user name: {}",user.getUserName());
-        String room = user.getRoomName();
-        log.info("user room: {}",room);
-        user.close();
-        if (!tutorsByRoomName.remove(room, user)){
-            log.info("I'm going to remove a student");
-            studentsByRoomName.get(room).remove(user);
-            log.info("I've removed a student");
-        }
-        log.info("the user has been removed from the '{}' room", room);
-    }
-    
-    
-    
-    
-	
 }
