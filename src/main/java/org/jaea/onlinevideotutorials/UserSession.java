7c7b571f5e6228fcb2a3bc8cb91d219313afa984
Juan Antonio <juanan.echeve@gmail.com>
2016-09-14 23:41:56 +0200

    Update UserSession.java

--- src/main/java/org/jaea/onlinevideotutorials/UserSession.java
+++ src/main/java/org/jaea/onlinevideotutorials/UserSession.java
@@ -1,103 +1,110 @@
 /*
- * (C) Copyright 2015 Kurento (http://kurento.org/)
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package org.jaea.onlinevideotutorials;
 
 import com.google.gson.JsonObject;
-import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-public class UserSession {
+/**
+ *
+ * @author juanan
+ */
+public class UserSession implements User{
     
-    private final Logger log = LoggerFactory.getLogger(UserSession.class);
+    protected final Logger log = LoggerFactory.getLogger(ParticipantSession.class);
     
-    public static final String TUTOR_TYPE = "tutor";
-    public static final String STUDENT_TYPE = "student";
-            
-    private WebSocketSession session;
-    private String userName;
-    private String userType;
-    private String roomName;
+    private final String name;
+    private final String userName;
+    private final String userType;
     
-    public UserSession(WebSocketSession session){
-        this.session = session;
-       
-    }
-    
-    public UserSession(WebSocketSession session, String userName, String userType){
-        this.session = session;
-        this.userName = userName;
-        this.userType = userType;
-        if (userType.equals(TUTOR_TYPE)){
-            roomName = userName;
-        }
-    }
-    
-    public UserSession(WebSocketSession session, String userName, String userType, String roomName){
+    private final WebSocketSession session;
+
+    public UserSession(WebSocketSession session, String userName, String userType, String name){
+        log.info("% UserSession {}", Hour.getTime());
+
+        this.name = name;
         this.session = session;
         this.userName = userName;
         this.userType = userType;
-        this.roomName = roomName;
+        
+        log.info("/ UserSession: {} {}", name, Hour.getTime());
+    }
+    
+    public UserSession(UserSession user){
+        log.info("% UserSession {}", Hour.getTime());
+
+        this.name = user.getName();
+        this.session = user.getSession();
+        this.userName = user.getName();
+        this.userType = user.getUserType();
+        
+        log.info("/ UserSession: {} {}", name, Hour.getTime());
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getUserName() {
+        return this.userName;
     }
 
+    public String getUserType() {
+        return this.userType;
+    }
+
+    public boolean isATutor(){
+        return this.userType.equals(TUTOR_TYPE);
+    }
+    
+    public boolean isAStudent(){
+        return this.userType.equals(STUDENT_TYPE);
+    }
+    
     public WebSocketSession getSession() {
         return session;
     }
     
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public String getRoomName() {
-        return roomName;
+    public String getSessionId() {
+        return session.getId();
     }
     
-    public void setUserName (String userName){
-        this.userName = userName;
-    }
-    
-    public void setRoomName (String roomName){
-        this.roomName = roomName;
-    }
-    
-    /**
-     * 
-     * @param message 
-     */
     public void sendMeAMessage(JsonObject message){
-        TextMessage textAnswer = new TextMessage(message.toString());
         
-        try{
-            log.info("sendMeAMessage: " + message.toString());
-            session.sendMessage(textAnswer);
-        }
-        catch(IOException e){
-            log.error("sendMeAMessage: {}", e.getMessage());
-        }
+        SendMessage.toClient(message, this.session);
     }
     
-    public void close(){
-        log.info("close");
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (this == obj) {
+            return true;
+	}
+	if ((obj == null) || !(obj instanceof User)) {
+            return false;
+	}
+	User other = (User) obj;
+	boolean eq = this.userName.equals(other.getUserName());
+
+	return eq;
     }
     
     
-	
+    
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+    */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + this.userName.hashCode();
+        return result;
+    }
 }
